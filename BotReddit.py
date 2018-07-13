9640d1f3c3eab9dd6a7656ac1a25403639c5a445
Darkbeast05 <41205601+Darkbeast05@users.noreply.github.com>
2018-07-13 14:30:52 -0400

    Update BotReddit.py

--- BotReddit.py
+++ BotReddit.py
@@ -1,8 +1,5 @@
 import praw, os, time
 from praw.models import MoreComments
-from boto.s3.connection import S3Connection
-
-s3 = S3Connection(os.environ['PASS'], os.environ['SECRET'])
 
 reddit = praw.Reddit(user_agent="Useless v0.1 by /u/PasswordPrevention", client_id='tsfUXGBvO_3ASw', client_secret=os.environ['SECRET'], username='UselessBotFF', password=os.environ['PASS'])
 sub = reddit.subreddit("testingground4bots")
