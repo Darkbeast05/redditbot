ba2e19388ffef41e99b26ae203a89a4374c195e4
Darkbeast05 <41205601+Darkbeast05@users.noreply.github.com>
2018-07-13 14:26:11 -0400

    Update BotReddit.py

--- BotReddit.py
+++ BotReddit.py
@@ -14,7 +14,7 @@ while True:
                 continue
             if "!summonbot1" in comment.body:
                 isme = False
-                print("found")
+                sys.stdout.write("found")
                 for reply in comment.replies:
                     if reply.author == reddit.redditor("UselessBotFF"):
                         isme=True
@@ -25,5 +25,5 @@ while True:
                         split.pop()
                         if split.pop() =='toHex':
                             comment.reply((''.join(hex(ord(x))[2:] for x in split.pop()) + "/n/n/n Am a bot."))
-                print(isme)
+                sys.stdout.write(isme)
                 time.sleep(20)
